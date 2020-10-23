08b4f70518c00f2708975b05ccddf4dba0aac25f
Alexandrusandy <68395084+Alexandrusandy@users.noreply.github.com>
2020-10-23 13:58:45 +0300

    a

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/', (req, res)=> { res.send('it is working') })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', signin.handleSignin(pool, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
