ea89c9ecc03d00451ac8bce9986b3d247a52f91a
Alexandrusandy <68395084+Alexandrusandy@users.noreply.github.com>
2020-10-23 14:21:56 +0300

    a

--- server.js
+++ server.js
@@ -9,36 +9,24 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const { Pool } = require('pg');
-const pool = new Pool({
+const { db } = require('pg');
+const db = new db({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
 const app = express();
 
 app.use(cors())
 app.use(bodyParser.json());
 
-
-app.post('/signin', signin.handleSignin(pool, bcrypt))
+app.get('/', (req, res)=> { res.send('it is working') })
+app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, pool, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, pool)})
-app.put('/image', (req, res) => { image.handleImage(req, res, pool)})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3001, ()=> {
