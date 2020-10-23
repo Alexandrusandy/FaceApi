724069869d02c4488b21d5ce7374a08120e34b1e
Alexandrusandy <68395084+Alexandrusandy@users.noreply.github.com>
2020-10-23 13:11:41 +0300

    try again

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ const db = knex({
   // connect to your own database here
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '',
-    database : 'smart_brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false
   }
+});
 
 const app = express();
 
