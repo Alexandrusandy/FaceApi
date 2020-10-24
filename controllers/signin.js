b107688c370bc9921fbaddc5d93cb61036aa2be7
Alexandrusandy <68395084+Alexandrusandy@users.noreply.github.com>
2020-10-24 12:51:42 +0300

    a

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,16 +1,16 @@
 
 
-const handleSignin=(db,bcrypt) => (req,res)=>{
+const handleSignin=(pool,bcrypt) => (req,res)=>{
 	const { email, password}= req.body;
 	if( !email || !password){
 		return res.status(400).json('Incorrect form submission');
 	}
-	db.select('email','hash').from('login')
+	pool.select('email','hash').from('login')
 	.where('email', '=',email )
 	.then(data => {
 	const isValid = bcrypt.compareSync(password,data[0].hash);
 		if (isValid) {
-		return db.select('*').from('users')
+		return pool.select('*').from('users')
 		.where('email','=',email)
 		.then(user => {
 			res.json(user[0])
