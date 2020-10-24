a73499abea8afab5876fc033caccca874181836a
Alexandrusandy <68395084+Alexandrusandy@users.noreply.github.com>
2020-10-24 13:28:01 +0300

    a

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const db = knex({
+ const db = knex({
   // connect to your own database here
   client: 'pg',
    connectionString: process.env.DATABASE_URL,
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send('it is working') })
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
